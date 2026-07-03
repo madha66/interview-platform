750742456d7d776203e7b66ce6c1a4a1e7390650
Madhan Kumar <madhanyuvi2009@gmail.com>
2026-07-03 20:59:50 +0530

    changes added

--- Interview-platform/backend/lib/oauth/google.js
+++ Interview-platform/backend/lib/oauth/google.js
@@ -1,10 +1,10 @@
 const { Google } = require("arctic");
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+require("dotenv").config({ path: path.resolve(__dirname, "../../../.env") });
 const google = new Google(
     process.env.Google_client_id,
     process.env.Google_client_password,
-    "https://interview-platform-93yk.onrender.com/google/callback"
+    process.env.GOOGLE_REDIRECT_URI || "https://interview-platform-93yk.onrender.com/google/callback"
 );
 
 module.exports = { google };
