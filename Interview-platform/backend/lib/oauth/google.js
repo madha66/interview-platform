40a96e4df52951afd456e3a4f814b996180b1f88
Madhan Kumar <madhanyuvi2009@gmail.com>
2026-07-03 21:01:13 +0530

    changes added

--- Interview-platform/backend/lib/oauth/google.js
+++ Interview-platform/backend/lib/oauth/google.js
@@ -4,7 +4,7 @@ require("dotenv").config({ path: path.resolve(__dirname, "../../../.env") });
 const google = new Google(
     process.env.Google_client_id,
     process.env.Google_client_password,
-    process.env.GOOGLE_REDIRECT_URI || "https://interview-platform-93yk.onrender.com/google/callback"
+    process.env.GOOGLE_REDIRECT_URI || "https://interview-platform-nine.vercel.app/google/callback"
 );
 
 module.exports = { google };
